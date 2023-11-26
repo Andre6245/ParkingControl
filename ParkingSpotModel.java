04f57309280a01b96e34a36da88bc869f3498230
André_Leocádio <leocadioandre2@gmail.com>
2023-11-26 14:23:44 -0300

    Add files via upload

---
+++ ParkingSpotModel.java
@@ -0,0 +1,10 @@
+package br.com.parking.parking.repository;
+
+import java.time.LocalDateTime;
+
+public class ParkingSpotModel {
+
+    public void setRegistrationDate(LocalDateTime now) {
+    }
+
+}
